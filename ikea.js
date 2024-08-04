6481ef0b69508516e7d6d8a89b4825e2a9c433d1
Nonya Biz Ness <164905463+linuxfandudeguy@users.noreply.github.com>
2024-08-04 12:27:39 -0400

    Update ikea.js

--- ikea.js
+++ ikea.js
@@ -134,16 +134,14 @@ function Text(content) {
   return document.createTextNode(content);
 }
 
-// Export functions if using ES modules (omit if using directly in HTML)
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = { 
-    A, Abbr, Address, Area, Article, Aside, Audio, B, Base, Bdi, Bdo, Blockquote, Body,
-    Br, Button, Canvas, Caption, Cite, Code, Col, Colgroup, Data, Datalist, Del, Details,
-    Dfn, Dialog, Div, Dl, Dt, Em, Embed, Fieldset, Figcaption, Figure, Footer, Form, H1, H2,
-    H3, H4, H5, H6, Head, Header, Hr, Html, I, Iframe, Img, Input, Ins, Kbd, Label, Legend,
-    Li, Link, Main, Map, Mark, Meta, Meter, Nav, Noscript, Object, Ol, Optgroup, Option,
-    Output, P, Param, Picture, Pre, Progress, Q, Rp, Rt, Ruby, S, Samp, Script, Section,
-    Select, Small, Source, Span, Strong, Style, Sub, Summary, Sup, Table, Tbody, Td,
-    Textarea, Tfoot, Th, Thead, Time, Title, Tr, U, Ul, Var, Video, Wbr, Text
-  };
-}
+// Attach functions to a global object (e.g., window)
+window.Ikea = {
+  A, Abbr, Address, Area, Article, Aside, Audio, B, Base, Bdi, Bdo, Blockquote, Body,
+  Br, Button, Canvas, Caption, Cite, Code, Col, Colgroup, Data, Datalist, Del, Details,
+  Dfn, Dialog, Div, Dl, Dt, Em, Embed, Fieldset, Figcaption, Figure, Footer, Form, H1, H2,
+  H3, H4, H5, H6, Head, Header, Hr, Html, I, Iframe, Img, Input, Ins, Kbd, Label, Legend,
+  Li, Link, Main, Map, Mark, Meta, Meter, Nav, Noscript, Object, Ol, Optgroup, Option,
+  Output, P, Param, Picture, Pre, Progress, Q, Rp, Rt, Ruby, S, Samp, Script, Section,
+  Select, Small, Source, Span, Strong, Style, Sub, Summary, Sup, Table, Tbody, Td,
+  Textarea, Tfoot, Th, Thead, Time, Title, Tr, U, Ul, Var, Video, Wbr, Text
+};
